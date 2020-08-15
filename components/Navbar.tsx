34095d15c67346722f3353852e2528bc18816818
NozomuTsuruta <tuno.0806@icloud.com>
2020-08-15 23:00:00 +0900

    'ベースの作成'

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,68 +1,56 @@
-import React from 'react';
+import React, { ReactNode, FC } from 'react';
+import { Navbar, NavDropdown, Nav } from 'react-bootstrap';
+import Link from 'next/link';
 
-const Navbar = () => {
+interface AppLinkProps {
+  children: ReactNode;
+  className: string;
+  href: string;
+}
+
+const AppLink:FC<AppLinkProps> = ({ children, className, href }) => (
+  <Link href={href}>
+    <a className={className}>{children}</a>
+  </Link>
+);
+
+const AppNavbar = () => {
   return (
     <>
       <div className='navbar-wrapper'>
-        <nav className='navbar navbar-expand-lg navbar-dark fj-mw9'>
-          <a className='navbar-brand mr-3 font-weight-bold' href='#'>
-            NozomuTsuruta
-          </a>
-          <button
-            className='navbar-toggler'
-            type='button'
-            data-toggle='collapse'
-            data-target='#navbarSupportedContent'
-            aria-controls='navbarSupportedContent'
-            aria-expanded='false'
-            aria-label='Toggle navigation'
-          >
-            <span className='navbar-toggler-icon'></span>
-          </button>
-
-          <div className='collapse navbar-collapse' id='navbarSupportedContent'>
-            <ul className='navbar-nav'>
-              <li className='nav-item mr-3'>
-                <a className='nav-link' href='#'>
-                  Portfolio
-                </a>
-              </li>
-              <li className='nav-item mr-3'>
-                <a className='nav-link' href='#'>
-                  Courses
-                </a>
-              </li>
-              <li className='nav-item mr-3'>
-                <a className='nav-link' href='#'>
-                  Cv
-                </a>
-              </li>
-              <li className='nav-item mr-3'>
-                <a className='nav-link' href='#'>
-                  Ask me
-                </a>
-              </li>
-            </ul>
-            <ul className='navbar-nav ml-auto'>
-              <li className='nav-item mr-3'>
-                <a className='nav-link' href='#'>
-                  Sign Up
-                </a>
-              </li>
-              <li className='nav-item mr-3'>
-                <a
-                  className='nav-link btn btn-success bg-green-2 bright'
-                  href='#'
-                >
-                  Sign In
-                </a>
-              </li>
-            </ul>
-          </div>
-        </nav>
+        <Navbar expand='lg' className='navbar-dark fj-mw9'>
+            <AppLink href='/' className='navbar-brand mr-3 font-weight-bold'>
+              NozomuTsuruta
+            </AppLink>
+          <Navbar.Toggle />
+          <Navbar.Collapse>
+            <Nav className='mr-auto'>
+              <AppLink href='/portfolios' className='nav-link mr-3'>
+                Portfolio
+              </AppLink>
+              <AppLink href='/forum/categories' className='nav-link mr-3'>
+                Courses
+              </AppLink>
+              <AppLink href='/cv' className='nav-link mr-3'>
+                Cv
+              </AppLink>
+            </Nav>
+            <Nav>
+              <AppLink href='/login' className='nav-link mr-3'>
+                Sign In
+              </AppLink>
+              <AppLink
+                href='/register'
+                className='nav-link mr-3 btn btn-success bg-green-2 bright'
+              >
+                Sign Up
+              </AppLink>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
       </div>
     </>
   );
 };
 
-export default Navbar;
+export default AppNavbar;
